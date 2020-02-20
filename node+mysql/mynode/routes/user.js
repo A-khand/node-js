c9a2ae4b84c7b2542ee29331882296d53f99ae2c
A-khand <38422805+A-khand@users.noreply.github.com>
2020-02-20 18:24:28 +0530

    Add files via upload

---
+++ node+mysql/mynode/routes/user.js
@@ -0,0 +1,92 @@
+
+//Mysql insert function
+exports.signup = (req, res) => {
+   message = '';
+   if(req.method == "POST"){
+      var post  = req.body;
+      var name= post.user_name;
+      var pass= post.password;
+      var fname= post.first_name;
+      var lname= post.last_name;
+      var mob= post.mob_no;
+
+      var sql = "INSERT INTO `users`(`first_name`,`last_name`,`mob_no`,`user_name`, `password`) VALUES ('" + fname + "','" + lname + "','" + mob + "','" + name + "','" + pass + "')";
+
+      var query = db.query(sql, (err, result) => {
+
+         message = "Succesfully! Your account has been created.";
+         res.render('index.ejs',{message: message});
+      });
+
+   } else {
+      res.render('signup');
+   }
+};
+ 
+//login page 
+exports.login = (req, res) => {
+   var message = '';
+   var sess = req.session; 
+
+   if(req.method == "POST"){
+      var post  = req.body;
+      var name= post.user_name;
+      var pass= post.password;
+     
+      var sql="SELECT id, first_name, last_name, user_name FROM `users` WHERE `user_name`='"+name+"' and password = '"+pass+"'";                           
+      db.query(sql, (err, results) => {      
+         if(results.length){
+            req.session.userId = results[0].id;
+            req.session.user = results[0];
+            console.log(results[0].id);
+            res.redirect('/home/dashboard');
+         }
+         else{
+            message = 'Wrong Credentials.';
+            res.render('index.ejs',{message: message});
+         }
+                 
+      });
+   } else {
+      res.render('index.ejs',{message: message});
+   }
+           
+};
+//dashboard page 
+           
+exports.dashboard = (req, res, next) => {
+           
+   var user =  req.session.user,
+   userId = req.session.userId;
+   console.log('Uid = '+userId);
+   if(userId == null){
+      res.redirect("/login");
+      return;
+   }
+
+   var sql="SELECT * FROM `users` WHERE `id`='"+userId+"'";
+
+   db.query(sql,(err, results) => {
+      res.render('dashboard.ejs', {user:user});    
+   });       
+};
+//logout functionality
+exports.logout=(req,res) => {
+   req.session.destroy((err)  => {
+      res.redirect("/login");
+   })
+};
+//render user details after login
+exports.profile = (req, res) => {
+
+   var userId = req.session.userId;
+   if(userId == null){
+      res.redirect("/login");
+      return;
+   }
+
+   var sql="SELECT * FROM `users` WHERE `id`='"+userId+"'";          
+   db.query(sql, (err, result) => {  
+      res.render('profile.ejs',{data:result});
+   });
+};
